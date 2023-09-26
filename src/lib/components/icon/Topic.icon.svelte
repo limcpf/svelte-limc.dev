db39930d71afbb771b00100eb4c2a066dab96be4
limcpf <matathresh1740@naver.com>
2023-09-26 17:21:11 +0900

    fix: 파일 누락 수정

---
+++ src/lib/components/icon/Topic.icon.svelte
@@ -0,0 +1,23 @@
+<svg width="15px" height="15px" enable-background="new 0 0 48 48" version="1.1" viewBox="0 0 48 48" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+
+<g fill="lightgray">
+
+<rect y="5.958" width="6" height="6"/>
+
+    <rect y="15.875" width="6" height="6"/>
+
+    <rect y="26.042" width="6" height="6"/>
+
+    <rect x="10" y="5.958" width="38" height="6"/>
+
+    <rect x="10" y="15.875" width="38" height="6"/>
+
+    <rect x="10" y="26.042" width="38" height="6"/>
+
+    <rect y="36.042" width="6" height="6"/>
+
+    <rect x="10" y="36.042" width="38" height="6"/>
+
+</g>
+
+</svg>

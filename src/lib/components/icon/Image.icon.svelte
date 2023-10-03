edb88b1cc6f585c0acbe128cac877804d82765f3
limcpf <matathresh1740@naver.com>
2023-10-03 22:17:45 +0900

    fix: 아이콘 누락

---
+++ src/lib/components/icon/Image.icon.svelte
@@ -0,0 +1,9 @@
+<svg width="15px" height="15px" version="1.1" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+    <g fill="none" fill-rule="evenodd">
+        <g transform="translate(-220 -3839)" fill="darkgray">
+            <g transform="translate(56 160)">
+                <path d="m166 3695h16v-14h-16v14zm-2 4h20v-20h-20v20z"></path>
+            </g>
+        </g>
+    </g>
+</svg>
---
+++ src/lib/components/icon/Loading.icon.svelte
@@ -0,0 +1,14 @@
+<svg width="100px" height="100px" display="block" shape-rendering="auto" style="background:none;margin:auto" preserveAspectRatio="xMidYMid" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <rect x="17.5" y="30" width="15" height="40" fill="#85a2b6">
+        <animate attributeName="y" begin="-0.2s" calcMode="spline" dur="1s" keySplines="0 0.5 0.5 1;0 0.5 0.5 1" keyTimes="0;0.5;1" repeatCount="indefinite" values="18;30;30"/>
+        <animate attributeName="height" begin="-0.2s" calcMode="spline" dur="1s" keySplines="0 0.5 0.5 1;0 0.5 0.5 1" keyTimes="0;0.5;1" repeatCount="indefinite" values="64;40;40"/>
+    </rect>
+    <rect x="42.5" y="30" width="15" height="40" fill="#bbcedd">
+        <animate attributeName="y" begin="-0.1s" calcMode="spline" dur="1s" keySplines="0 0.5 0.5 1;0 0.5 0.5 1" keyTimes="0;0.5;1" repeatCount="indefinite" values="20.999999999999996;30;30"/>
+        <animate attributeName="height" begin="-0.1s" calcMode="spline" dur="1s" keySplines="0 0.5 0.5 1;0 0.5 0.5 1" keyTimes="0;0.5;1" repeatCount="indefinite" values="58.00000000000001;40;40"/>
+    </rect>
+    <rect x="67.5" y="30" width="15" height="40" fill="#dce4eb">
+        <animate attributeName="y" calcMode="spline" dur="1s" keySplines="0 0.5 0.5 1;0 0.5 0.5 1" keyTimes="0;0.5;1" repeatCount="indefinite" values="20.999999999999996;30;30"/>
+        <animate attributeName="height" calcMode="spline" dur="1s" keySplines="0 0.5 0.5 1;0 0.5 0.5 1" keyTimes="0;0.5;1" repeatCount="indefinite" values="58.00000000000001;40;40"/>
+    </rect>
+</svg>

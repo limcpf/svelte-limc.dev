6cb120fc0118af68fe12ac32150533abfa9745f9
limcpf <matathresh1740@naver.com>
2023-10-04 20:24:24 +0900

    fix: form 타입으로 변경

--- src/lib/components/admin/Login.svelte
+++ src/lib/components/admin/Login.svelte
@@ -4,43 +4,61 @@
     let id = "";
     let pw = "";
 
-    function login() {
-        fetch(`${import.meta.env.VITE_API_SERVER_URL}/public/login`, {
+    async function login(e: SubmitEvent) {
+        e.preventDefault();
+
+        const url = `${import.meta.env.VITE_API_SERVER_URL}/public/login`;
+        const body = {
+            name: id,
+            password: pw
+        };
+
+        const req = await fetch(url, {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                name: id,
-                password: pw
-            }),
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(body),
             credentials: "include"
-        })
-        .then((result) => result.json())
-        .then((json) => {
-            console.log(json);
-            location.reload();
-        })
-        .catch((e: any) => {
-            console.log(e);
-            alert(e.message || "로그인 실패!")
-        })
+        });
+
+        if(req.ok) {
+            const loginDto = await req.json()
+            if(loginDto.accessToken) location.reload();
+            return;
+        }
+
+        console.log(e);
+        alert(e.message || "로그인 실패!");
     }
 </script>
 
 <div class="login-wrapper">
-    <InputForm title="아이디" bind:value={id} />
-    <InputForm title="비밀번호" bind:value={pw} type="password"/>
-    <button on:click={() => {login()}}>로그인</button>
+    <form on:submit={login}>
+        <InputForm title="아이디" bind:value={id} />
+        <InputForm title="비밀번호" bind:value={pw} type="password"/>
+
+        <button class="login-btn" type="submit">로그인</button>
+    </form>
 </div>
 
 
 <style>
+    form {
+        width: 50%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-content: center;
+    }
     .login-wrapper {
         width: 100%;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
+    }
+    .login-btn {
+        width: 100%;
+        padding: 1em;
+        margin-top: 1em;
     }
 </style>

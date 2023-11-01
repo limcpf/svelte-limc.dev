3f7158f7779ea0fb2457a58dc24b0573785cb975
limcpf <matathresh1740@naver.com>
2023-11-01 22:44:15 +0900

    fix: description add

--- src/routes/post/[id]/+page.svelte
+++ src/routes/post/[id]/+page.svelte
@@ -11,5 +11,6 @@
     <meta content={"https://limc.dev/post/" + data.postTitle.title.replaceAll(" ", "-")} property="og:url">
     <meta content={data.postTitle.title} property="og:title">
     <meta content={data.postTitle.summary.slice(0, 150)} property="og:description">
+    <meta content={data.postTitle.summary.slice(0, 150)} property="description">
 </svelte:head>
 <Post data={data}/>

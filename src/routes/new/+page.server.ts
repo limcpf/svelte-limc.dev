f6a853df46e2d151376a52074729eca119e8b086
limcpf <matathresh1740@naver.com>
2023-11-21 12:47:55 +0900

    feat : new test

---
+++ src/routes/new/+page.server.ts
@@ -0,0 +1,12 @@
+import type PostReadDto from "$lib/domain/Post/PostRead.dto";
+
+export async function load() {
+    const req = await fetch(`${import.meta.env.VITE_API_SERVER_URL}/public/post/site/DEV/recent`)
+    const postReadDto = await req.json() as PostReadDto;
+
+    return {
+        props: {
+            postReadDto : postReadDto
+        }
+    };
+}
---
+++ src/routes/new/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import Post from "$lib/components/post/Post.svelte";
+    import type PostReadDto from "$lib/domain/Post/PostRead.dto";
+
+    /** @type {import('./$types').PageData} */
+    export let data: { props : {
+        postReadDto : PostReadDto
+    }};
+
+    const postReadDto = data.props.postReadDto;
+</script>
+
+<svelte:head>
+    <title>싱싱한 코딩</title>
+</svelte:head>
+
+<div class="recent-header"><span>최신 글</span><a href="/post">글 목록</a></div>
+<Post data={postReadDto} />
+
+<style>
+    div.recent-header {
+        width: 100%;
+        max-width: 1024px;
+        border-bottom: 1px solid darkgrey;
+        align-items: end;
+        padding-bottom: 0.3em;
+        display: flex;
+        justify-content: space-between;
+    }
+    div.recent-header > span {
+        font-size:1.3em;
+        font-weight: bold;
+    }
+    div.recent-header > a {
+        text-decoration: underline;
+    }
+</style>

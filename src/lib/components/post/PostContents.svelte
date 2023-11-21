38343d9fb7943b64a80e9b436feb97dff0d85b77
limcpf <matathresh1740@naver.com>
2023-11-21 23:19:08 +0900

    fix: post content component

--- src/lib/components/post/PostContents.svelte
+++ src/lib/components/post/PostContents.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import type PostContentsDto from "$lib/domain/Post/PostContents.dto";
-    import PostContentMd from "$lib/components/post/contents/PostContentMd.svelte";
     import PostTableOfContents from "$lib/components/post/contents/PostTableOfContents.svelte";
+    import NewPostContentMd from "$lib/components/new/post/contents/NewPostContentMd.svelte";
 
     export let postContents: PostContentsDto;
     const { content, tableOfContent } = postContents;
@@ -9,5 +9,5 @@
 
 <article class="markdown-body">
     <PostTableOfContents tableOfContents={tableOfContent} />
-    <PostContentMd content={content} />
+    <NewPostContentMd content={content} />
 </article>

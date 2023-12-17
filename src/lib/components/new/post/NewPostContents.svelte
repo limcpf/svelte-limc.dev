baf1808dd1fd326f7a5c1a6f8e0588b57738d449
limcpf <matathresh1740@naver.com>
2023-12-17 15:47:24 +0900

    fix: new test

--- src/lib/components/new/post/NewPostContents.svelte
+++ src/lib/components/new/post/NewPostContents.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import type PostContentsDto from "$lib/domain/Post/PostContents.dto";
-    import PostTableOfContents from "$lib/components/post/contents/PostTableOfContents.svelte";
     import NewPostContentMd from "$lib/components/new/post/contents/NewPostContentMd.svelte";
 
     export let postContents: PostContentsDto;
@@ -8,6 +7,5 @@
 </script>
 
 <article class="markdown-body">
-    <PostTableOfContents tableOfContents={tableOfContent} />
     <NewPostContentMd content={content} />
 </article>
